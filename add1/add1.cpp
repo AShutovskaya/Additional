2c5c329f798c73e1fd39bf926aeb8a8c91529776
AShutovskaya <93682589+AShutovskaya@users.noreply.github.com>
2022-06-23 23:28:50 +0800

    Add files via upload

---
+++ add1/add1.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+int reverse(int n)
+{
+	if (n / 10 == 0)
+		return n;
+	else
+	{
+		int x = int(log10(n));
+		x = pow(10, x);
+		return (n % 10)*x + reverse(n / 10);
+	}
+
+}
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int y = rand() % 1000000 + 1;
+		cout << y << ' ' << reverse(y) << endl;
+
+
+	}
+		
+	return 0;
+
+}
